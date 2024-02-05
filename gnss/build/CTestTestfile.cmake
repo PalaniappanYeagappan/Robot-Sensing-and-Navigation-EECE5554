7f89b1078ad6574da83ec63f6f214f5203782c96
Palaniappan Yeagappan <153960947+PalaniappanYeagappan@users.noreply.github.com>
2024-02-05 12:20:05 -0500

    Update commit

--- gnss/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/palaniappan_yeagappan/gnss/src
-# Build directory: /home/palaniappan_yeagappan/gnss/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("gps_driver")
